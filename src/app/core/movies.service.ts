a17f4f41c844bcfd5264257ce7672a217b8f2ea1
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2021-01-25 00:35:02 -0300

    Created movies service and save method added

---
+++ src/app/core/movies.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Movie } from '../shared/models/movies';
+import { Observable } from 'rxjs';
+
+const url = 'http://localhost:3000/movies';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  save(movie: Movie): Observable<Movie> {
+    return this.http.post<Movie>(url, movie);
+  }
+}
--- src/app/movies/register-movies/register-movies.component.ts
+++ src/app/movies/register-movies/register-movies.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { Movie } from '../../shared/models/movies';
+import { MoviesService } from '../../core/movies.service';
 import { ValidateFieldService } from '../../shared/services/validate-field.service';
+
+import { Movie } from '../../shared/models/movies';
 
 @Component({
   selector: 'app-register-movies',
@@ -12,7 +14,11 @@ import { ValidateFieldService } from '../../shared/services/validate-field.servi
 export class RegisterMoviesComponent implements OnInit {
   formRegister: FormGroup;
 
-  constructor(private fb: FormBuilder, public validate: ValidateFieldService) {}
+  constructor(
+    private fb: FormBuilder,
+    public validate: ValidateFieldService,
+    private moviesService: MoviesService
+  ) {}
 
   get f() {
     return this.formRegister.controls;
@@ -48,7 +54,18 @@ export class RegisterMoviesComponent implements OnInit {
       return;
     }
 
-    alert(JSON.stringify(this.formRegister.value, null, 4));
+    const movie = this.formRegister.getRawValue() as Movie;
+
+    this.save(movie);
+  }
+
+  private save(movie: Movie): void {
+    this.moviesService.save(movie).subscribe(() => {
+      alert('OK');
+
+      this.onResetForm();
+    }),
+      () => alert('ERRO');
   }
 
   onResetForm(): void {
