516b197a55ec2b8efe97b7770ff94a9da1598d54
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2021-01-26 17:14:53 -0300

    Added input to filtered of listing

--- src/app/movies/list-movies/list-movies.component.ts
+++ src/app/movies/list-movies/list-movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { MoviesService } from 'src/app/core/movies.service';
 import { Movie } from '../../shared/models/movies';
@@ -9,17 +10,32 @@ import { Movie } from '../../shared/models/movies';
   styleUrls: ['./list-movies.component.scss'],
 })
 export class ListMoviesComponent implements OnInit {
+  formListing: FormGroup;
   movies: Movie[];
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(private fb: FormBuilder, private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.initialForm();
     this.get();
+    this.filter();
+  }
+
+  initialForm(): void {
+    this.formListing = this.fb.group({
+      texto: [''],
+    });
   }
 
   get(): void {
     this.moviesService
       .getAll()
       .subscribe((movies: Movie[]) => (this.movies = movies));
+  }
+
+  filter(): void {
+    this.formListing.valueChanges.subscribe((value: string) => {
+      console.log('Detectado');
+    });
   }
 }
