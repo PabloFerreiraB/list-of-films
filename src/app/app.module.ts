8ef347e3fd02dcfee216b6296ae276318a2a7320
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2021-02-01 18:50:46 -0300

    Added loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './shared/material/material.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AlertComponent } from './shared/ui/alert/alert.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
 
 @NgModule({
   declarations: [
@@ -28,9 +30,13 @@ import { FooterComponent } from './shared/footer/footer.component';
     HttpClientModule,
     LayoutModule,
     MaterialModule,
+    NgxSpinnerModule,
   ],
   entryComponents: [AlertComponent],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt' },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
